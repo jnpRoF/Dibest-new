aa373412d267e884b3078ce9e3a6fc09c004c13a
Johnson <ogbonnayajprof2015@gmail.com>
2023-12-04 00:23:17 +0100

    make nav responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,18 @@
+import {useState} from "react"
+import { Link } from "react-router-dom";
 import images from "../../constants/images";
 import Button from "../Button/Button";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+// import "../../index.css";
 
 const NavBar = ({ getFormState, display }) => {
   const handleFormDisplay = () => {
-    console.log("true");
     getFormState(!display);
+  };
+
+   const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const onMenuClick = () => {
+    setMenuOpen(!isMenuOpen);
   };
 
   return (
@@ -21,7 +27,7 @@ const NavBar = ({ getFormState, display }) => {
             className="focus:outline-none p-1"
           />
         </div> */}
-        <div className="flex space-x-7">
+        <div className="hidden lg:flex space-x-7">
           <a className="hover:text-gray-300" href="#">
             Home
           </a>
@@ -41,7 +47,62 @@ const NavBar = ({ getFormState, display }) => {
             Contact Us
           </a>
         </div>
-        <div className="flex space-x-3">
+        {/* Hamburger */}
+        <button
+          id="menu-btn"
+          className={`block hamburger lg:hidden focus:outline-none ${
+            isMenuOpen === false ? "" : "open"
+          }`}
+          onClick={onMenuClick}
+        >
+          <span class="hamburger-top"></span>
+          <span class="hamburger-middle"></span>
+          <span class="hamburger-bottom"></span>
+        </button>
+        <div
+          id="menu"
+          className={`absolute flex flex-col items-center self-end py-6 top-24 space-y-4 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md ${
+            isMenuOpen === false ? "" : "flex"
+          } ${isMenuOpen === false ? "hidden" : ""}`}
+        >
+          <a
+            className="hover:border border-gold rounded-full p-2 px-8"
+            href="#"
+          >
+            Home
+          </a>
+          <a
+            className="hover:border border-gold rounded-full p-2 px-8"
+            href="#features"
+          >
+            Features
+          </a>
+          <a
+            className="hover:border border-gold rounded-full p-2 px-8"
+            href="#pricing"
+          >
+            Pricing
+          </a>
+          <a
+            className="hover:border border-gold rounded-full p-2 px-8"
+            href="#about"
+          >
+            About
+          </a>
+          <Link
+            className="hover:border border-gold rounded-full p-2 px-8"
+            to="platformvendor"
+          >
+            Platform Vendors
+          </Link>
+          <a
+            className="hover:border border-gold rounded-full p-2 px-8"
+            href="#contact"
+          >
+            Contact Us
+          </a>
+        </div>
+        <div className="space-x-3 hidden lg:flex">
           <Button
             className="p-2 px-5 bg-purple text-white rounded-md text-sm hover:opacity-90"
             onClick={handleFormDisplay}
