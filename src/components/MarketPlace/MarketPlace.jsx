ca7634133fd67d429bab472f6b009187ef0bcc39
Johnson <ogbonnayajprof2015@gmail.com>
2023-12-04 16:43:12 +0100

    tried to fix slider

--- src/components/MarketPlace/MarketPlace.jsx
+++ src/components/MarketPlace/MarketPlace.jsx
@@ -6,19 +6,26 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useState } from "react";
 import "../../index.css";
+import images from "../../constants/images";
 
 const MarketPlace = () => {
   console.log(marketplaces);
-  const [activeFaceId, setActiveFaceId] = useState(1);
+  const [activeFaceId, setActiveFaceId] = useState(0);
 
   const handleButtonClick = (id) => {
     setActiveFaceId(id);
   };
 
   const settings = {
+    // speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: true,
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+    // afterChange: (index) => {
+    //   console.log("current slide", index);
+    // },
   };
   return (
     <>
@@ -27,10 +34,10 @@ const MarketPlace = () => {
           DiBest Spot Marketplaces
         </h4>
         <div className="flex flex-wrap px-4 lg:space-x-3 justify-center lg:justify-between">
-          {/* {marketplaces.map((eachMarketPlace) => {
+          {marketplaces.map((eachMarketPlace) => {
             return (
               <Button
-                className={`bg-darkPink p-2 px-8 m-2 ${
+                className={`${eachMarketPlace.btnColor} p-2 px-8 m-2 ${
                   activeFaceId === eachMarketPlace.id ? "active" : ""
                 }`}
                 key={eachMarketPlace.id}
@@ -41,8 +48,8 @@ const MarketPlace = () => {
                 {eachMarketPlace.headText}
               </Button>
             );
-          })} */}
-          <Button className="bg-restaurantsColor p-2 px-8 m-2">
+          })}
+          {/* <Button className="bg-restaurantsColor p-2 px-8 m-2">
             <a
               href="http://local.dibestspot.com"
               target="_blank"
@@ -69,23 +76,52 @@ const MarketPlace = () => {
           <Button className="bg-carrRegionColor p-2 px-8 m-2">
             Caribbean Region
           </Button>
-          <Button className="bg-amber-500 p-2 px-8 m-2">Medicals</Button>
+          <Button className="bg-amber-500 p-2 px-8 m-2">Medicals</Button> */}
         </div>
       </div>
-      <div className="flex space-x-2 p-5 overflow-x-hidden">
-        {/* <Slider {...settings}>
-          {marketplaces.map((eachMarketPlace) => {
-            return (
+      {/* <div className="flex space-x-2 p-5 "> */}
+      {/* <div className=""> */}
+      <Slider {...settings}>
+        {marketplaces.map((eachMarketPlace, index) => {
+          return (
+            <div
+              className="flex flex-col h-auto p-7 lg:p-6 lg:py-8 border rounded-md w-[90%] lg:h-[420px] lg:w-4/5 justify-between shrink-0 lg:translate-x-[20px] lg:flex-row"
+              key={index}
+            >
               <MarketPlaceCard
                 key={eachMarketPlace.id}
                 headText={eachMarketPlace.headText}
                 bodyText={eachMarketPlace.bodyText}
                 btnColor={eachMarketPlace.btnColor}
               />
-            );
-          })}
-        </Slider> */}
-        <MarketPlaceCard
+              {/* <>
+                <img
+                  src={images.christmas}
+                  alt=""
+                  className="h-full w-full lg:w-[49%]"
+                />
+                <div className="flex flex-col space-y-4 lg:space-y-3 lg:h-full w-full lg:w-[49%] lg:justify-between">
+                  <h3 className="font-semibold text-2xl py-3 lg:text-3xl">
+                    {eachMarketPlace.headText}
+                  </h3>
+                  <div className="flex flex-col space-y-3">
+                    <p className="text-base">{eachMarketPlace.bodyText}</p>
+                  </div>
+                  <div className="">
+                    <Button
+                      className={`p-2 px-8 ${eachMarketPlace.btnColor} rounded-sm text-white`}
+                    >
+                      LEARN MORE
+                    </Button>
+                  </div>
+                </div>
+              </> */}
+            </div>
+          );
+        })}
+      </Slider>
+      {/* </div> */}
+      {/* <MarketPlaceCard
           headText="Restaurants"
           bodyText="Savor Success and Boost Your Restaurant's Visibility: Enroll as a Vendor in the Ultimate Restaurant Marketplace!Are you a culinary maestro looking to tantalize taste buds or a food enthusiast eager to explore the finest dining experiences? Welcome to our Restaurant Marketplace, where flavors, innovation, and community converge. Whether you're a restaurant owner or a foodie on the hunt for the next delectable dish, this is your gastronomic haven."
           btnColor="bg-restaurantsColor"
@@ -124,10 +160,51 @@ const MarketPlace = () => {
           headText="Medicals"
           bodyText="Empower Your Wellness Journey: Are you ready to experience a new standard of healthcare and revolutionize the way you access medical services? Then embark on a journey towards personalized, convenient, and top-notch healthcare. Discover a world where your well-being is top priority. Sign Up Now as a DiBest Member for FREE! Take the first step toward a healthier, happier you. Our platform is designed to make quality healthcare accessible to everyone."
           btnColor="bg-amber-500"
-        />
-      </div>
+        /> */}
+      {/* </div> */}
     </>
   );
 };
 
 export default MarketPlace;
+
+// {
+//   activeFaceId === eachMarketPlace.id && (
+// <>
+//   <img src={images.christmas} alt="" className="h-full w-full lg:w-[49%]" />
+//   <div className="flex flex-col space-y-4 lg:space-y-3 lg:h-full w-full lg:w-[49%] lg:justify-between">
+//     <h3 className="font-semibold text-2xl py-3 lg:text-3xl">
+//       {eachMarketPlace.headText}
+//     </h3>
+//     <div className="flex flex-col space-y-3">
+//       <p className="text-base">{eachMarketPlace.bodyText}</p>
+//     </div>
+//     <div className="">
+//       <Button
+//         className={`p-2 px-8 ${eachMarketPlace.btnColor} rounded-sm text-white`}
+//       >
+//         LEARN MORE
+//       </Button>
+//     </div>
+//   </div>
+// </>
+//   );
+// }
+// <>
+//   <img src={images.christmas} alt="" className="h-full w-full lg:w-[49%]" />
+//   <div className="flex flex-col space-y-4 lg:space-y-3 lg:h-full w-full lg:w-[49%] lg:justify-between">
+//     <h3 className="font-semibold text-2xl py-3 lg:text-3xl">
+//       {eachMarketPlace.headText}
+//     </h3>
+//     <div className="flex flex-col space-y-3">
+//       <p className="text-base">{eachMarketPlace.bodyText}</p>
+//     </div>
+//     <div className="">
+//       <Button
+//         className={`p-2 px-8 ${eachMarketPlace.btnColor} rounded-sm text-white`}
+//       >
+//         LEARN MORE
+//       </Button>
+//     </div>
+//   </div>
+// </>;
