293ef5ff1fa4692b3a35c7a080ab5fbdd1d7bc8a
Johnson <ogbonnayajprof2015@gmail.com>
2023-12-07 05:25:16 +0100

    fix space

--- src/components/SectionHead/SectionHead.jsx
+++ src/components/SectionHead/SectionHead.jsx
@@ -5,7 +5,7 @@ import "../../index.css";
 const SectionHead = () => {
   return (
     <div className="flex mt-[9rem] mb-5">
-      <div className="flex flex-col space-y-8 justify-end mx-auto w-4/5">
+      <div className="flex flex-col space-y-10 justify-end mx-auto w-4/5">
         {/* <div class="tooltip mx-auto">
           <span class="tooltiptext font-semibold text-xl">Coming Soon!</span>
         </div> */}
@@ -19,7 +19,7 @@ const SectionHead = () => {
         </div>
         <div className="flex w-full flex-col space-y-2 md:w-4/5 md:flex-row justify-center md:justify-between">
           <h2 className="text-2xl sm:text-[1.675rem] lg:text-3xl">
-            Vendors Directory
+            Platform Vendors
           </h2>
           <div className="flex space-x-2 ml-2 items-center">
             <IoIosFunnel />
