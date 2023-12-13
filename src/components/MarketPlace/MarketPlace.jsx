839394ae185bb6606ddb327749a65ea9c11a137e
Johnson <ogbonnayajprof2015@gmail.com>
2023-12-13 17:12:02 +0100

    fix marketplaces images

--- src/components/MarketPlace/MarketPlace.jsx
+++ src/components/MarketPlace/MarketPlace.jsx
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../index.css";
 
+
 const MarketPlace = () => {
   const settings = {
     speed: 500,
@@ -71,9 +72,10 @@ const MarketPlace = () => {
             >
               <MarketPlaceCard
                 key={eachMarketPlace.id}
+                src={eachMarketPlace.src}
                 headText={eachMarketPlace.headText}
                 bodyText={eachMarketPlace.bodyText}
-                btnColor={eachMarketPlace.btnColor}
+                btnColor={eachMarketPlace.btnColor }
               />
               {/* <>
                 <img
@@ -101,48 +103,6 @@ const MarketPlace = () => {
           );
         })}
       </Slider>
-      {/* </div> */}
-      {/* <MarketPlaceCard
-          headText="Restaurants"
-          bodyText="Savor Success and Boost Your Restaurant's Visibility: Enroll as a Vendor in the Ultimate Restaurant Marketplace!Are you a culinary maestro looking to tantalize taste buds or a food enthusiast eager to explore the finest dining experiences? Welcome to our Restaurant Marketplace, where flavors, innovation, and community converge. Whether you're a restaurant owner or a foodie on the hunt for the next delectable dish, this is your gastronomic haven."
-          btnColor="bg-restaurantsColor"
-        />
-        <MarketPlaceCard
-          headText="OnDemand Services"
-          bodyText="Be a Pioneer in the On-Demand Evolution: Secure Your Spot Today! Don't miss the opportunity to redefine your business. Join as a vendor today for your On-Demand business and take your services to new heights. DiBest Spot is not just a marketplace; it's a platform for growth, innovation, and success."
-          btnColor="bg-demColor"
-        />
-        <MarketPlaceCard
-          headText="Property Rental"
-          bodyText=" Unlock Your Property's Full Rental Potential : Join today as a vendor ready to take the next step in maximizing your property's rental potential? Enroll as a Vendor and experience the difference. Click the button to start your journey towards increased rental income and stress-free property management."
-          btnColor="bg-renColor"
-        />
-        <MarketPlaceCard
-          headText="Ecommerce"
-          bodyText='Supercharge Your E-Commerce Business: Unlock new markets, boost sales, and enjoy a host of benefits that will revolutionize your online business. Click the "Enroll as Vendor" button to revolutionize your online business with increased sales, broader reach, and unparalleled support. Discover a world of opportunities.'
-          btnColor="bg-darkPink"
-        />
-        <MarketPlaceCard
-          headText="Ewallet"
-          bodyText="Effortless Transactions with Enhanced Security:Say goodbye to cumbersome payment processes. Our Platform ensures quick and hassle-free transactions for both vendors and customers. Ready to transform the way you do business? Enroll as a Vendor Today and harness the power of our integrated platform."
-          btnColor="bg-ewalletColor"
-        />
-        <MarketPlaceCard
-          headText="Egift"
-          bodyText="Boost Your Brand: Leverage our marketplace to elevate your brand's presence. Benefit from increased exposure, customer reviews, and word-of-mouth marketing that will establish your business as a trusted and reliable choice. Enroll as a Vendor Today and harness the power of our integrated platform."
-          btnColor="bg-gold"
-        />
-        <MarketPlaceCard
-          headText="Caribbean Region"
-          bodyText="Discover the Caribbean Paradise and Enjoy An Authentic Caribbean Experience: Are you ready to immerse yourself in the vibrant colors, rich culture, and breathtaking landscapes of the Caribbean? Welcome to our Caribbean Marketplace, where vendors and users come together to celebrate the beauty of this tropical haven. Whether you're a local business or an avid explorer, this is your gateway to unforgettable events, enticing tourism packages, and captivating attractions."
-          btnColor="bg-carrRegionColor"
-        />
-        <MarketPlaceCard
-          headText="Medicals"
-          bodyText="Empower Your Wellness Journey: Are you ready to experience a new standard of healthcare and revolutionize the way you access medical services? Then embark on a journey towards personalized, convenient, and top-notch healthcare. Discover a world where your well-being is top priority. Sign Up Now as a DiBest Member for FREE! Take the first step toward a healthier, happier you. Our platform is designed to make quality healthcare accessible to everyone."
-          btnColor="bg-amber-500"
-        /> */}
-      {/* </div> */}
     </>
   );
 };
--- src/constants/marketplaces.js
+++ src/constants/marketplaces.js
@@ -3,7 +3,8 @@ const marketplaces = [
     id: 1,
     headText: "Restaurants",
     bodyText:
-      "Savor Success and Boost Your Restaurant's Visibility: Enroll as a Vendor in the Ultimate Restaurant Marketplace!Are you a culinary maestro looking to tantalize taste buds or a food enthusiast eager to explore the finest dining experiences? Welcome to our Restaurant Marketplace, where flavors, innovation, and community converge. Whether you're a restaurant owner or a foodie on the hunt for the next delectable dish, this is your gastronomic haven.",
+      "Savor Success and Boost Your Restaurant's Visibility: Enroll as a Vendor in the Ultimate Restaurant Marketplace! Are you a culinary maestro looking to tantalize taste buds or a food enthusiast eager to explore the finest dining experiences? Welcome to our Restaurant Marketplace, where flavors, innovation, and community converge. Whether you're a restaurant owner or a foodie on the hunt for the next delectable dish, this is your gastronomic haven.",
+    src: "../../assets/frame4.jpg",
     btnColor: "bg-restaurantsColor",
   },
   {
@@ -11,13 +12,15 @@ const marketplaces = [
     headText: "OnDemand Services",
     bodyText:
       "Be a Pioneer in the On-Demand Evolution: Secure Your Spot Today! Don't miss the opportunity to redefine your business. Join as a vendor today for your On-Demand business and take your services to new heights. DiBest Spot is not just a marketplace; it's a platform for growth, innovation, and success.",
+    src: "../../assets/card4.jfif",
     btnColor: "bg-demColor",
   },
   {
     id: 3,
     headText: "Property Rental",
     bodyText:
-      " Unlock Your Property's Full Rental Potential : Join today as a vendor ready to take the next step in maximizing your property's rental potential? Enroll as a Vendor and experience the difference. Click the button to start your journey towards increased rental income and stress-free property management.",
+      " Unlock Your Property's Full Rental Potential: Join today as a vendor ready to take the next step in maximizing your property's rental potential? Enroll as a Vendor and experience the difference. Click the button to start your journey towards increased rental income and stress-free property management.",
+    src: "../../assets/property.jpg",
     btnColor: "bg-renColor",
   },
   {
@@ -25,13 +28,15 @@ const marketplaces = [
     headText: "Ecommerce",
     bodyText:
       'Supercharge Your E-Commerce Business: Unlock new markets, boost sales, and enjoy a host of benefits that will revolutionize your online business. Click the "Enroll as Vendor" button to revolutionize your online business with increased sales, broader reach, and unparalleled support. Discover a world of opportunities.',
+    src: "../../assets/card1.jfif",
     btnColor: "bg-darkPink",
   },
   {
     id: 5,
     headText: "Ewallet",
     bodyText:
-      "Effortless Transactions with Enhanced Security:Say goodbye to cumbersome payment processes. Our Platform ensures quick and hassle-free transactions for both vendors and customers. Ready to transform the way you do business? Enroll as a Vendor Today and harness the power of our integrated platform.",
+      "Effortless Transactions with Enhanced Security: Say goodbye to cumbersome payment processes. Our Platform ensures quick and hassle-free transactions for both vendors and customers. Ready to transform the way you do business? Enroll as a Vendor Today and harness the power of our integrated platform.",
+    src: "../../assets/ewallet.jpg",
     btnColor: "bg-ewalletColor",
   },
   {
@@ -39,6 +44,7 @@ const marketplaces = [
     headText: "Egift",
     bodyText:
       "Boost Your Brand: Leverage our marketplace to elevate your brand's presence. Benefit from increased exposure, customer reviews, and word-of-mouth marketing that will establish your business as a trusted and reliable choice. Enroll as a Vendor Today and harness the power of our integrated platform.",
+    src: "../../assets/group2.jfif",
     btnColor: "bg-gold",
   },
   {
@@ -46,6 +52,7 @@ const marketplaces = [
     headText: "Caribbean Region",
     bodyText:
       "Discover the Caribbean Paradise and Enjoy An Authentic Caribbean Experience: Are you ready to immerse yourself in the vibrant colors, rich culture, and breathtaking landscapes of the Caribbean? Welcome to our Caribbean Marketplace, where vendors and users come together to celebrate the beauty of this tropical haven. Whether you're a local business or an avid explorer, this is your gateway to unforgettable events, enticing tourism packages, and captivating attractions.",
+    src: "../../assets/frame3.jfif",
     btnColor: "bg-carrRegionColor",
   },
   {
@@ -53,6 +60,7 @@ const marketplaces = [
     headText: "Medical",
     bodyText:
       "Empower Your Wellness Journey: Are you ready to experience a new standard of healthcare and revolutionize the way you access medical services? Then embark on a journey towards personalized, convenient, and top-notch healthcare. Discover a world where your well-being is top priority. Sign Up Now as a DiBest Member for FREE! Take the first step toward a healthier, happier you. Our platform is designed to make quality healthcare accessible to everyone.",
+    src: "../../assets/medical-industry.jpg",
     btnColor: "bg-amber-500",
   },
 ];
